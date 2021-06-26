cd01939b43fa87636d9227267425015da72c5022
AyushGupta57 <ayushgupta12122001@gmail.com>
2021-06-26 14:13:53 +0530

    last commit

--- Covid/blaze.php
+++ Covid/blaze.php
@@ -18,16 +18,17 @@
                 <img src="resources/img/logo.png" alt="logo" class="logo">
                 <ul class="main-nav">
                     <li><a href="#sec-features">HOME</a></li>
-                    <li><a href="#">ABOUT</a></li>
+                    <li><a href="#foot">ABOUT</a></li>
                     <li><a href="#symptoms">SYMPTOMS</a></li>
-                    <li><a href="#">CONTACT</a></li>
+                    <li><a href="#foot">CONTACT</a></li>
+                    <li><a href="https://selfregistration.cowin.gov.in/" target="_blank">Co-WIN</a></li>
                 </ul>
             </div>
         </nav>
         <div class="hero-box">
             <h1>Lets Stay Safe.<br>Fight Together Against Corona Virus.</h1>
-            <a class="btn btn-full" href="#">World's Live Update </a>
-            <a class="btn btn-half" href="#">India's Live Update </a>
+            <a class="btn btn-full" href="WR/index.php" target="_blank">World's Live Update </a>
+            <a class="btn btn-half" href="https://b65b98133198.ngrok.io/" target="_blank">India's Live Update </a>
         </div>
     </header>
  
@@ -41,26 +42,26 @@
         </div>
         <div class="row" align='center'>
             <div class="col span-1-of-3 box">
-                <a href="#" class="iconbtn"><img src="resources/img/a.png" alt="vaccine"></a>
-                <a href="#" class="btn btn-full">XXX</a>
+                <a href="https://selfregistration.cowin.gov.in/" class="iconbtn" target="_blank"><img src="resources/img/a.png" alt="vaccine"></a>
+                
             </div>
             <div class="col span-1-of-3 box">
-                <a href="#" class="iconbtn"><img src="resources/img/b.png" alt="vaccine"></a>
-                <a href="#" class="btn btn-full">XXX</a>
+                <a href="WR/oxy.php" class="iconbtn" target="_blank"><img src="resources/img/b.png" alt="oxygen"></a>
+                
             </div>
             <div class="col span-1-of-3 box">
-                <a href="#" class="iconbtn"><img src="resources/img/c.png" alt="vaccine"></a>
-                <a href="#" class="btn btn-full">XXX</a>
+                <a href="WR/availbed.php" class="iconbtn" target="_blank"><img src="resources/img/c.png" alt="bed"></a>
+                
             </div>
         </div>
         <div class="row" align='center'>
             <div class="col span-1-of-2 box">
-                <a href="#" class="iconbtn"><img src="resources/img/a.png" alt="vaccine"></a>
-                <br><a href="#" class="btn btn-full">XXX</a>
+                <a href="WR/plasma.php" class="iconbtn" target="_blank"><img src="resources/img/blood.jpeg" alt="plasma"></a>
+                <br>
             </div>
             <div class="col span-1-of-2 box">
-                <a href="#" class="iconbtn"><img src="resources/img/a.png" alt="vaccine"></a>
-                <br><a href="#" class="btn btn-full">XXX</a>
+                <a href="WR/drugs.php" class="iconbtn" target="_blank"><img src="resources/img/drug.jpg" alt="drug"></a>
+                <br>
             </div>
             
         </div>
@@ -158,6 +159,11 @@
 
         </div>
 
+    </div>
+
+    </div>
+    <div align='right'>
+    <a class="btn btn-half" href="https://amzn.to/2U5sWMC" target="_blank">Purchase Mask</a>
     </div>
 
 </section>
@@ -284,7 +290,9 @@
         </div>
 
     </div>
-
+    <div align='right'>
+    <a class="btn btn-half" href="https://amzn.to/3vXFNOm" target="_blank">Purchase Hand Wash </a>
+    </div>
 </section>
 
 <!-- hand-wash section ends -->
@@ -303,83 +311,63 @@
 <!--BLAZE END-->
 
 <!--FOOTER STARTS-->
-    <footer>
+    <footer id="foot">
         <div class="row">
             <div class="col span-1-of-2">
                 <ul class="footer-nav">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Symptoms</a></li>
-                    <li><a href="#">Contacts</a></li>
+                    <li><a href="#sec-features">Home</a></li>
+                    <li><a href="#prevent">Preventions</a></li>
+                    <li><a href="#symptoms">Symptoms</a></li>
+                    <li><a href="#precautions">Precautions</a></li>
+                </ul>
+                <ul>
+                <P>
+                    <br>---------------------------------------------------------------------------------------------------------<br>
+                    <u><b><i>A B O U T</i></b></u>&nbsp &nbsp
+                    <u><b><i>U S  </i></b></u>
+                </P>
+                <p>
+                    We are a group of 4 individuals from Inderprastha Engineering College, Ghaziabad, U.P., dedicated to acknowledge all your queries and bring all the resources relating to Covid-19, under one roof.
+                </p>
                 </ul>
             </div>
-            <div class="col span-1-of-2">
+            <div class="col span-1-of-3">
+            <p>
+                    <u><b><i>C O N T A C T</i></b></u>&nbsp &nbsp
+                    <u><b><i>U S  </i></b></u> 
+                    <br>---------------------------------------------------------------------<br>
+            </p>
                 <ul class="social-links">
-                    <li><p>Ayush Kumar Singh :- </li>
+                    <li><p>Ayush Kumar Singh :- </p></li>
                     <li><a href="https://www.facebook.com/ayush.r.90" target="_blank"><i class="ion-social-facebook"></i></a></li>
                     <li><a href="https://twitter.com/AyushKu63458226?s=09" target="_blank"><i class="ion-social-twitter"></i></a></li>
-                    <li><a href="https://www.linkedin.com/in/ayush-kumar-singh-a40937188"><i class="ion-social-linkedin"></i></a></li>
+                    <li><a href="https://www.linkedin.com/in/ayush-kumar-singh-a40937188" target="_blank"><i class="ion-social-linkedin"></i></a></li>
                     <li><a href="https://instagram.com/ayush_the_one.90?igshid=f9l43itiw9gb" target="_blank"><i class="ion-social-instagram"></i></a></li>
                 </ul>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col span-1-of-2">
-                <ul class="footer-nav">
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
-            </div>
-            <div class="col span-1-of-2">
                 <ul class="social-links">
-                    <li><p>Devangi Verma :- </li>
+                    <li><p>Devangi Verma :-</p></li>
                     <li><a href="#" target="_blank"><i class="ion-social-facebook"></i></a></li>
                     <li><a href="https://twitter.com/verma_devangi" target="_blank"><i class="ion-social-twitter"></i></a></li>
-                    <li><a href="https://www.linkedin.com/in/devangi-verma-231585207/"><i class="ion-social-linkedin"></i></a></li>
+                    <li><a href="https://www.linkedin.com/in/devangi-verma-231585207/" target="_blank"><i class="ion-social-linkedin"></i></a></li>
                     <li><a href="https://www.instagram.com/devangi.verma/" target="_blank"><i class="ion-social-instagram"></i></a></li>
                 </ul>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col span-1-of-2">
-                <ul class="footer-nav">
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
-            </div>
-            <div class="col span-1-of-2">
                 <ul class="social-links">
-                    <li><p>Akanksha :- </li>
+                    <li><p>Akanksha :- </p></li>
                     <li><a href="#" target="_blank"><i class="ion-social-facebook"></i></a></li>
                     <li><a href="#" target="_blank"><i class="ion-social-twitter"></i></a></li>
-                    <li><a href="https://www.linkedin.com/in/itsjustakanksha"><i class="ion-social-linkedin"></i></a></li>
+                    <li><a href="https://www.linkedin.com/in/itsjustakanksha" target="_blank"><i class="ion-social-linkedin"></i></a></li>
                     <li><a href="https://www.instagram.com/itsjustakanksha/" target="_blank"><i class="ion-social-instagram"></i></a></li>
                 </ul>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col span-1-of-2">
-                <ul class="footer-nav">
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
-            </div>
-            <div class="col span-1-of-2">
                 <ul class="social-links">
-                    <li><p>Ayush Gupta :- </li>
+                    <li><p>Ayush Gupta :- </p></li>
                     <li><a href="https://www.facebook.com/profile.php?id=100009290680053" target="_blank"><i class="ion-social-facebook"></i></a></li>
                     <li><a href="https://mobile.twitter.com/AyushGupta1212" target="_blank"><i class="ion-social-twitter"></i></a></li>
-                    <li><a href="https://www.linkedin.com/in/ayush-gupta-1489711a1"><i class="ion-social-linkedin"></i></a></li>
+                    <li><a href="https://www.linkedin.com/in/ayush-gupta-1489711a1" target="_blank"><i class="ion-social-linkedin"></i></a></li>
                     <li><a href="https://www.instagram.com/invites/contact/?i=frn60wn0pgmc&utm_content=24lju9m" target="_blank"><i class="ion-social-instagram"></i></a></li>
                 </ul>
             </div>
         </div>
+            
         <div class="row">
             <p>
                 Copyright &copy; 2021 by BLAZE. All rights reserved.
